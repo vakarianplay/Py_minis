602b4f187fb3bd5f688762adb08a059db9097e28
Kostya Vakarian <vakarianplay@gmail.com>
2025-12-09 14:46:17 +0300

    Threading work

--- PyRTSPRecorder/recorder_main.py
+++ PyRTSPRecorder/recorder_main.py
@@ -3,8 +3,7 @@ import time
 import yaml
 import logging
 import subprocess
-from threading import Thread
-
+from threading import Thread, current_thread
 
 def setup_global_logging(log_file):
     log_dir = os.path.dirname(log_file)
@@ -12,11 +11,11 @@ def setup_global_logging(log_file):
         os.makedirs(log_dir)
 
     logging.basicConfig(
-        level=logging.INFO, 
-        format="%(asctime)s [%(levelname)s] %(message)s",
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] [Thread-%(thread)d] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
-            logging.FileHandler(log_file),  
+            logging.FileHandler(log_file),
             logging.StreamHandler()
         ]
     )
@@ -26,13 +25,13 @@ class CameraRecorder(Thread):
         super().__init__()
         self.name = name
         self.rtsp_url = rtsp_url
-        self.output_folder = output_folder  
+        self.output_folder = output_folder
         self.segment_time = segment_time
-        self.running = False  
+        self.running = False
         os.makedirs(self.output_folder, exist_ok=True)
 
     def run(self):
-        unix_time = int(time.time()) 
+        unix_time = int(time.time())
         output_template = os.path.join(
             self.output_folder,
             f"{unix_time}+%Y-%m-%d_%H-%M-%S.mp4"
@@ -52,25 +51,25 @@ class CameraRecorder(Thread):
             "-strftime", "1",          # Time in filename
             "-reset_timestamps", "1",  # Reset timestamps for containers
             "-metadata", f"description={self.name} {unix_time}",
-            "-metadata", f"creation_time={time}",
+            "-metadata", f"creation_time={unix_time}",
             output_template
         ]
 
-        logging.info(f"Init record '{self.name}' src: '{self.output_folder}'")
+        logging.info(f"Сamera '{self.name}' Thread ID: {current_thread().ident}")
         self.running = True
         try:
             subprocess.run(ffmpeg_command, check=True)
         except KeyboardInterrupt:
-            logging.warning(f"'{self.name}' stoped")
+            logging.warning(f"Recording for camera '{self.name}' stopped manually.")
         except Exception as e:
-            logging.error(f"Error from '{self.name}': {e}")
+            logging.error(f"Error from camera '{self.name}': {e}")
         finally:
             self.running = False
-            logging.info(f"Record complete for '{self.name}'")
+            logging.info(f"Thread completed for camera '{self.name}' with ID: {current_thread().ident}")
 
     def stop_recording(self):
-        logging.info(f"Stop record for '{self.name}'")
-        self.running = False
+        logging.info(f"Stopping recording for camera '{self.name}'")
+        self.running = False  # Update internal state if required
 
 class MultiCameraRecorder:
     def __init__(self, config_file):
@@ -85,20 +84,18 @@ class MultiCameraRecorder:
         return config
 
     def start_recording(self):
-
-        logging.info("Start recording")
+        logging.info(f"Starting recording for all cameras. Main Thread ID: {current_thread().ident}")
 
         for camera in self.config.get("cameras", []):
             name = camera["name"]
             rtsp_url = camera["rtsp_url"]
-            # output_folder = camera["output_folder"]
             output_folder = self.config.get("output_folder", "cam") + "/" + name
             recorder = CameraRecorder(name, rtsp_url, output_folder, self.segment_time)
             self.recorders.append(recorder)
             recorder.start()
 
     def stop_recording(self):
-        logging.info("Stop recording")
+        logging.info("Stopping recording for all cameras.")
         for recorder in self.recorders:
             recorder.stop_recording()
 
@@ -110,18 +107,19 @@ def main():
             config = yaml.safe_load(f)
         log_file = config.get("log_file", "record_rtsp.log")
     except Exception as e:
-        print(f"Error load config.yml: {e}")
+        print(f"Error loading config.yml: {e}")
         return
 
+    # Setup logging
     setup_global_logging(log_file)
     recorder_manager = MultiCameraRecorder(CONFIG_FILE)
 
     try:
         recorder_manager.start_recording()
         while True:
-            time.sleep(1) 
+            time.sleep(1)
     except KeyboardInterrupt:
-        logging.info("Stop manually.")
+        logging.info("Stopping recording manually.")
         recorder_manager.stop_recording()
 
 if __name__ == "__main__":
